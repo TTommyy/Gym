bb319430fb84b09d66142ca871adec3920307f82
Tommy Hoang <tommyyhoangg@gmail.com>
2020-12-07 19:47:23 +0100

    Added a fucntional hamburger for mobile and added link animations

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ const burger = document.querySelector(".burger");
 const burgerM = document.querySelector(".menu-burger");
 
 burger.addEventListener("click", () => {
-   burger.classList.toggle(".open-burger");
-   burgerM.classList.toggle(".open-menu");
+   burger.classList.toggle("open");
+   burgerM.classList.toggle("openM");
 });

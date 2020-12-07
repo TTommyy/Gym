11dc6ccb0f8661c0b400f2a53adda59bfb75361a
Tommy Hoang <tommyyhoangg@gmail.com>
2020-12-07 20:11:06 +0100

    added animations to ladning page

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Hamburger for mobile
+// Hamburger for mobile and tablet
 const burger = document.querySelector(".burger");
 const burgerM = document.querySelector(".menu-burger");
 
@@ -6,3 +6,14 @@ burger.addEventListener("click", () => {
    burger.classList.toggle("open");
    burgerM.classList.toggle("openM");
 });
+
+// Animation for home - desktop
+const tl = gsap.timeline({defaults: {ease: "power1.out"}});
+
+// Navbar
+tl.from(".nav", {y: "-100%", duration: 1, opacity: 0})
+
+// Landing page
+tl.from(".lp-left-slope", {x: "-75%", duration: 1.2, opacity: 0})
+tl.from(".lp-content", {x: "25%", duration: 1.2, opacity: 0})
+tl.from(".socials", {x: "75%", duration: 1, opacity: 0, delay: 0.2})

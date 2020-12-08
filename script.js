80bb2eeae765d59b63d2b66c7c3642c975f14772
Tommy Hoang <tommyyhoangg@gmail.com>
2020-12-08 18:09:26 +0100

    Added animations to the what we offer section

--- script.js
+++ script.js
@@ -14,6 +14,31 @@ const tl = gsap.timeline({defaults: {ease: "power1.out"}});
 tl.from(".nav", {y: "-100%", duration: 1, opacity: 0})
 
 // Landing page
-tl.from(".lp-left-slope", {x: "-75%", duration: 1.2, opacity: 0})
-tl.from(".lp-content", {x: "25%", duration: 1.2, opacity: 0})
-tl.from(".socials", {x: "75%", duration: 1, opacity: 0, delay: 0.2})
+tl.from(".lp-left-slope", {x: "-55%", duration: 0.8, opacity: 0})
+tl.from(".lp-content h4, .lp-content h1, .lp-text-d, .lp-button", {y: "50%", duration: 1, opacity: 0, stagger: 0.2}, "-=0.2");
+tl.from(".socials", {x: "30%", duration: 1, opacity: 0}, "-=0.5")
+
+const lpButton = document.querySelector(".lp-button");
+
+lpButton.addEventListener("mouseover", () => {
+   lpButton.style.transform = "translateY(-4px)";
+   lpButton.style.transition = "ease 500ms";
+});
+
+lpButton.addEventListener("mouseout", () => {
+   lpButton.style.transform = "translateY(4px)";
+   lpButton.style.transition = "ease 500ms";
+});
+
+// What we offer section
+let wwo = gsap.timeline({
+   scrollTrigger: {
+      trigger: ".section-3",
+      start: "center bottom"
+   }
+});
+
+wwo.from(".wwo-slope", {x: "-100%", opacity: 0, duration: 1});
+wwo.from(".wwo-content", {x: "50%", opacity: 0, duration: 1}, "-=1");
+wwo.from(".wwo-slope-d", {y: "65%", opacity: 0, duration: 1.25});
+wwo.from(".wwo-text1, .wwo-text2", {y: "-65%", opacity: 0, duration: 1, stagger: 0.25}, "-=1");
